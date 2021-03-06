2165c782b9c16cd2e71568b739a7572f9777fb66
Gabriel De Castro Torres <gabrielctorres38@gmail.com>
2021-03-05 23:12:54 -0300

    Criando movimentação do personagem, pulo esta bugado

---
+++ Assets/Scripts/Personagens/Alexandre.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alexandre : Personagem
+{
+    
+    public override void Start()
+    {
+       
+    }   
+
+
+    public override void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void Ataque()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Personagens/Classes Pai/Personagem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Personagem : MonoBehaviour
+{
+    public float vida;
+    public float velocidade;
+    public float forcaPulo;
+    public float dano;
+    private Rigidbody2D rb2d;
+    private FloorColision colisaoChao;
+
+    public virtual void Start()
+    {
+        colisaoChao = GetComponentInChildren<FloorColision>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public virtual void FixedUpdate()
+    {
+        Andar();
+        Pular();
+    }
+
+    public void Andar()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 dir = new Vector2(horizontal, 0);
+        rb2d.velocity = dir.normalized * velocidade;
+    }
+
+    public void Pular()
+    {
+        if (Input.GetButtonDown("Jump") && colisaoChao.estaNoChao)
+        {
+            rb2d.AddForce(new Vector2(rb2d.velocity.x,forcaPulo),ForceMode2D.Impulse);
+        }
+    }
+
+    public abstract void Ataque();
+
+}
