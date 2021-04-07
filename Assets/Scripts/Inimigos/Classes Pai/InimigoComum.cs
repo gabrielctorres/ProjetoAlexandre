31fa1deba28d36bc3f66aa4130fdd1e98ec8e9ff
Mateus Pinheiro <mateuzfp25@gmail.com>
2021-04-07 16:40:36 -0300

    Implementação da IA do marinheiro e seus ataques

--- Assets/Scripts/Inimigos/Classes Pai/InimigoComum.cs
+++ Assets/Scripts/Inimigos/Classes Pai/InimigoComum.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public abstract class InimigoComum : MonoBehaviour
 {
+    float taxaAtaque = 1;
+    float proximoAtaque = 0;
+
     private float velocidadeDoInimigo = 3;
 
     private Transform posicaoDoJogador;
 
     private float distancia;
+    
+    private static Rigidbody2D rb;
+
+    protected Animator spriteAnimation;
+
     // Start is called before the first frame update
     public virtual void Start()
     {
         posicaoDoJogador = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        spriteAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,25 +33,39 @@ public abstract class InimigoComum : MonoBehaviour
 
     public virtual void FixedUpdate()
     {
-        seguirJogador();
+        SeguirJogador();        
     }
 
-    void seguirJogador()
+    void SeguirJogador()
     {
         if (posicaoDoJogador.gameObject != null)
         {
             distancia = Vector2.Distance(this.gameObject.transform.position, posicaoDoJogador.position);
 
-            if (distancia >= 2)
+            if (distancia >= 1.7f)
             {
                 velocidadeDoInimigo = 3;
                 this.transform.position = Vector2
                 .MoveTowards(this.gameObject.transform.position, new Vector2(posicaoDoJogador.transform.position.x, posicaoDoJogador.transform.position.y), velocidadeDoInimigo * Time.deltaTime);
+                spriteAnimation.SetBool("podeAndar", true);
+                spriteAnimation.SetBool("podeAtacar", false);
             }
-            else if(distancia <= 2)
+            else if(distancia <= 1.7f)
             {
                 velocidadeDoInimigo = 0;
-            }            
+                AtacarJogador();                
+            }
         }
     }
+
+    void AtacarJogador()
+    {
+        if (Time.time > proximoAtaque)
+        {
+            proximoAtaque = Time.time + taxaAtaque;
+            spriteAnimation.SetBool("podeAtacar", true);
+        }
+        else
+            spriteAnimation.SetBool("podeAtacar", false);
+    }    
 }
