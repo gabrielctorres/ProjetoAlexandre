c47b00d3135a9983b05c019ab858b9a2e513a836
Gabriel De Castro Torres <gabrielctorres38@gmail.com>
2021-04-14 16:01:04 -0300

    Adicionando Fundo das fases, parallax e iluminacao

---
+++ Assets/Scripts/Cenas/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float tamanho, primeiraPos;
+
+    public GameObject cam;
+
+    public float parallaxEffect;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        primeiraPos = transform.position.x;
+        tamanho = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(primeiraPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > primeiraPos + tamanho) primeiraPos += tamanho;
+        else if (temp < primeiraPos - tamanho) primeiraPos -= tamanho;
+
+    }
+}
