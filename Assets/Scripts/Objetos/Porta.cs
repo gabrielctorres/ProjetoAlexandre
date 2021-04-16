4a9d9b32238847ce8d151a19a8c727031b02bc8b
Gabriel De Castro Torres <gabrielctorres38@gmail.com>
2021-04-16 16:19:39 -0300

    tilemap das fases alterado e parallax corrigido

---
+++ Assets/Scripts/Objetos/Porta.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porta : MonoBehaviour
+{
+    public Transform posicaoSaida;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Personagem>() != null)
+        {
+            collision.transform.position = posicaoSaida.position;
+        }
+    }
+}
