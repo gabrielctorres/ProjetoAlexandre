4ccf42d4ec2532550b2684c1196705680ff8deaf
Mateus Pinheiro <mateuzfp25@gmail.com>
2021-06-09 14:13:43 -0300

    Finalizando a implementação da aranha

---
+++ Assets/Scripts/Inimigos/Aranha.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aranha : InimigoComum
+{   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();            
+        dano = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();        
+    }
+
+    private void FixedUpdate()
+    {
+        base.FixedUpdate();        
+    }       
+}
