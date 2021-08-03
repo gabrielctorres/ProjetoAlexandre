5768dba82e38cfcd5cf1dd13af1169965a9326b9
Mateus Pinheiro <mateuzfp25@gmail.com>
2021-08-03 18:08:20 -0300

    Prefabs dos objetos corrigidos

--- Assets/Scripts/Objetos/PlataformaBalestra.cs
+++ Assets/Scripts/Objetos/PlataformaBalestra.cs
@@ -9,15 +9,14 @@ public class PlataformaBalestra : MonoBehaviour
 
     [Header("Definições da balestra correspondente")]
     public GameObject projetil;
+    public Transform balestra;
     public Transform spawnProjetil;
-    public int direcaoOlhar;
-
+    
     // Start is called before the first frame update
     void Start()
     {
         posX = GetComponent<Transform>().transform.position.x;        
-        posY = GetComponent<Transform>().transform.position.y;
-        direcaoOlhar = -1;
+        posY = GetComponent<Transform>().transform.position.y;        
     }
 
     // Update is called once per frame
@@ -31,8 +30,11 @@ public class PlataformaBalestra : MonoBehaviour
         if(collision.tag == "Player")
         {
             GetComponent<Rigidbody2D>().MovePosition(new Vector2(posX, posY-0.1f));
-            GameObject projetilInstanciado = Instantiate(projetil, spawnProjetil.position, transform.rotation);
-            projetilInstanciado.GetComponent<Rigidbody2D>().velocity = transform.right * -10f;
+            GameObject projetilInstanciado = Instantiate(projetil, spawnProjetil.position, balestra.rotation);            
+
+            projetilInstanciado.GetComponent<Rigidbody2D>().velocity = (spawnProjetil.position-balestra.transform.position) * 30f;
+
+            Destroy(projetilInstanciado, 5f);
         }
     }
 
--- Assets/Scripts/Objetos/PlataformaQuebravel.cs
+++ Assets/Scripts/Objetos/PlataformaQuebravel.cs
@@ -14,7 +14,7 @@ public class PlataformaQuebravel : MonoBehaviour
     void Update()
     {
         
-    }  
+    }    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -22,11 +22,43 @@ public class PlataformaQuebravel : MonoBehaviour
         {
             StartCoroutine(Quebrar());
         }
-    }    
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Reativar());
+        }
+    }
 
     IEnumerator Quebrar()
     {
-        yield return new WaitForSeconds(1f);
-        gameObject.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+
+        if (gameObject.GetComponent<SpriteRenderer>().enabled)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        if (gameObject.GetComponent<BoxCollider2D>().enabled)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        } 
+    }
+
+    IEnumerator Reativar()
+    {
+        yield return new WaitForSeconds(3f);
+
+        if (gameObject.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        if (gameObject.GetComponent<BoxCollider2D>().enabled == false)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
     }
 }
