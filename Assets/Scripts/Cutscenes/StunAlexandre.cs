1fbe2d81ab5ae76432521a96bac2e99d2b694e3b
Mateus Pinheiro <mateuzfp25@gmail.com>
2021-09-28 16:05:38 -0300

    Ajustes finais na cutscene final fase 1

---
+++ Assets/Scripts/Cutscenes/StunAlexandre.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunAlexandre : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        this.GetComponent<ParticleSystem>().Play();
+    }
+}
